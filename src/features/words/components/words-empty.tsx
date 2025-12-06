345510b4a6b40e1b743ee59a02d9d4ac45aa79ce
MertenD <merten.dieckmann@web.de>
2025-12-06 19:57:50 +0100

    Make word empty functional

--- src/features/words/components/words-empty.tsx
+++ src/features/words/components/words-empty.tsx
@@ -1,17 +1,17 @@
 import {EmptyView} from "@/components/entity-components";
 import {useCreateWord} from "@/features/words/hooks/use-words";
 import {useUpgradeModal} from "@/hooks/use-upgrade-modal";
+import {useState} from "react";
+import {CreateWordInput} from "@/features/words/schema/word-crud-schema";
+import {WordCreateDialog} from "@/features/words/components/word-create-dialog";
 
 export default function WordsEmpty() {
     const createWord = useCreateWord()
     const { handleError, modal } = useUpgradeModal()
+    const [isOpen, setIsOpen] = useState(false)
 
-    // TODO Create real word item
-    const handleCreate = () => {
-        createWord.mutate({
-            german: "Brot",
-            serbian: "Hleb"
-        }, {
+    const handleCreate = (newWord: CreateWordInput) => {
+        createWord.mutate(newWord, {
             onError: (error) => {
                 handleError(error)
             }
@@ -21,8 +21,9 @@ export default function WordsEmpty() {
     return <>
         {modal}
         <EmptyView
-            onNew={handleCreate}
+            onNew={() => setIsOpen(true)}
             message="No words found. Get started by adding a word!"
         />
+        <WordCreateDialog open={isOpen} onOpenChange={setIsOpen} onCreate={handleCreate} />
     </>
 }
